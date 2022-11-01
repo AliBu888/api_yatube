eca4f53fdcaaac3bea92fdcde247c09e188ae9dd
Albina Trifonova <Businka.ATO@ya.ru>
2022-11-01 10:57:13 +0300

    Sprint 8 - review 2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,11 +1,11 @@
 import datetime as dt
 
-from django.shortcuts import get_object_or_404
+from .permissions import IsAuthorOrReadOnly
 from posts.models import Group, Post
 from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 
-from .permissions import IsOwnerOrReadOnly
 from .serializers import (CommentsSerializer, GroupSerializer,
                           PostListSerializer)
 
@@ -13,15 +13,14 @@ from .serializers import (CommentsSerializer, GroupSerializer,
 class PostsViewSet(viewsets.ModelViewSet):
     """ViewSet для получения списка всех постов и создания нового.
     Редактирования и удаления только своего поста."""
-    queryset = Post.objects.all()
+    queryset = Post.objects.select_related('author')
     serializer_class = PostListSerializer
-    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly,)
 
     def perform_create(self, serializer):
         """Автозаполнение полей author и pub_date"""
         serializer.save(
             author=self.request.user,
-            pub_date=dt.date.today()
         )
 
 
@@ -34,13 +33,12 @@ class CommentsViewSet(viewsets.ModelViewSet):
     """ViewSet для получения списка всех комментариев и создания нового.
     Редактирования и удаления только своих комментариев."""
     serializer_class = CommentsSerializer
-    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly,)
 
     def get_queryset(self):
         """Этот view возвращает комментарии поста с id из запроса."""
-        # return Comment.objects.filter(post__id=self.kwargs['post_id'])
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
-        return post.comments.all()
+        return post.comments.select_related('author')
 
     def perform_create(self, serializer):
         """Автозаполнение полей author и created"""
